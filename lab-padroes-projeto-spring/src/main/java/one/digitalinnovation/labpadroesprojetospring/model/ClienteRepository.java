e6215a7293584bde37bcbec4ffe9cf16a19a4033
Daniela Monteiro <danimdf360@gmail.com>
2022-01-30 17:12:28 -0300

    Project design patterns

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/labpadroesprojetospring/model/ClienteRepository.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.labpadroesprojetospring.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+    Iterable<Cliente> findAll();
+
+    void save(Cliente cliente);
+
